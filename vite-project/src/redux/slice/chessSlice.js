cf05c768ad8e086839f12c5b212f01ed36ce9349
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-05-30 10:58:43 +0300

    Crud İslemleri İcin GetAll,GetById ve Post İşlemi Yapıldı

--- vite-project/src/redux/slice/chessSlice.js
+++ vite-project/src/redux/slice/chessSlice.js
@@ -10,4 +10,39 @@ const initalState={
 }
     
 
+const fetchChessList=createAsyncThunk("chess/getAll",async(payload,{rejectWithValue,getState,dispatch})=>{
+        try{
+            const response=await axios.get(apiUrl);
+            return response.chess_players
+        }
 
+        catch(error){
+
+            rejectWithValue(error.response.status)
+        }
+})
+
+
+const fetchChessGetById=createAsyncThunk("chess/getById",async (payload,{rejectWithValue,state,dispatch})=>{
+    try{
+        const response=await axios.get(`${apiUrl}/${id}`);
+        return response.chess_players;
+    }
+
+    catch(error){
+        rejectWithValue(error.status.code);
+    }
+})
+
+
+const createChess=createAsyncThunk("chess/create",async(payload,{rejectWithValue,state,dispatch})=>{
+
+    try{
+       const response= await axios.post(`${apiUrl}`,payload);
+       return response.chess_players
+    }
+
+    catch(error){
+        rejectWithValue(error.status.code);
+    }
+})
