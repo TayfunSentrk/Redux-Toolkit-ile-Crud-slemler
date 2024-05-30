db22922736c75e4696d1ce9956910b7179f55e3f
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-05-30 12:15:13 +0300

    Form Componenti Oluşturuldu ve State'ler tanımlandı

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ChessList from './components/ChessList/ChessList'
+import Form from './components/Form/Form'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Form/>
       <ChessList/>
     </>
   )
