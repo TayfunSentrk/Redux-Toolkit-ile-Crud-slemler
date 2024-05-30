63478f6f7cf1a7f742649d4152008772744fbdd4
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-05-30 12:04:26 +0300

    ChessList ve Chess Item 'de css düzenlemeleri yapıldı

--- vite-project/src/components/ChessItem/ChessItem.jsx
+++ vite-project/src/components/ChessItem/ChessItem.jsx
@@ -5,10 +5,15 @@ const ChessItem = ({item}) => {
     const {name,age,biography,image_url}=item
   return (
     <div className={styles.container}>
-            <h3>${name}</h3>
-            <h4>${age}</h4>
-            <p>${biography}</p>
+            <h3>{name}</h3>
+            <h4>{age}</h4>
+            <p>{biography}</p>
             <img src={image_url}/>
+
+            <div>
+              <button className={styles.delete}>Sil</button>
+              <button className={styles.update}>Düzenle</button>
+            </div>
     </div>
   )
 }
